3656f3565feb143fe20c39f32f1eda729f0d4bf4
Khushal <khushalmajoka21@gmail.com>
2025-05-04 12:47:24 +0530

    Initial Commit

---
+++ server/index.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+dotenv.config();
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('OrderNest API running!');
+});
+
+const PORT = process.env.PORT || 5000;
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => app.listen(PORT, () => console.log(`Server running on ${PORT}`)))
+  .catch((err) => console.log(err));

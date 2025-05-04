a78fb5c9ad5b0c70f1818084353c8b9c4194c76e
Sourabh Majoka <majokasourabh30@gmail.com>
2025-05-04 15:48:13 +0530

    login page updated

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -6,11 +6,9 @@ const Login = () => {
     <div className="min-h-screen flex items-center justify-evenly bg-gradient-to-br from-orange-100 to-gray-100 px-4 font-poppins">
       <img src={OrderNestLogo} alt="OrderNest Logo" className="" />
       <div className="max-w-md w-full bg-white p-8 rounded-2xl shadow-md">
-        <div className="flex flex-col items-center mb-6">
-          <h1 className="text-3xl font-bold text-gray-800 mb-2">
-            Welcome to OrderNest
-          </h1>
-          <p className="text-sm text-gray-500">Login to your store dashboard</p>
+        <div className="flex flex-col items-center ">
+          <h1 className="text-2xl font-semibold text-gray-800">Welcome to OrderNest</h1>
+          <p className="text-sm text-gray-500 mt-1">Login to your store dashboard</p>
         </div>
 
         <form className="space-y-5">
@@ -33,6 +31,9 @@ const Login = () => {
               placeholder="••••••••"
               className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-orange-400"
             />
+            <div className="text-right text-sm text-orange-600 hover:underline cursor-pointer">
+              Forgot password?
+            </div>
           </div>
           <button
             type="submit"
